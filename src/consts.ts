4aae09cd485b58bbfd8623e9b619ca5f0fc6b142
blamm01 <blam2010.hn@gmail.com>
2024-01-14 18:30:35 +0700

    refactor: refactor the code and replace plain html with astro

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+export const Links = {
+    discord: 'https://discord.com/users/736636650796351559',
+    facebook: 'https://facebook.com/blamm01',
+    x: 'https://x.com/blamm01',
+    email: 'mailto:blam@nhblam.space',
+    github: 'https://github.com/blamm01'
+}
