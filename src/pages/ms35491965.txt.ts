2206ac1d415f5de984c917d9a98f6ca87eac0308
blamm01 <blam2010.hn@gmail.com>
2024-03-14 14:10:18 +0700

    add ms verify for blam.id.vn

---
+++ src/pages/ms35491965.txt.ts
@@ -0,0 +1,11 @@
+﻿import type { APIRoute } from 'astro';
+
+const response = `
+{
+  "Description": "Domain ownership verification file for Microsoft 365 - place in the website root",
+  "Domain": "blam.id.vn",
+  "Id": "337bde94-0ac0-4a40-a235-b5e1d12f4fa2"
+}
+`
+
+export const GET: APIRoute = () => new Response(response)
