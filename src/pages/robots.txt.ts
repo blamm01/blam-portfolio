25482690e1da4f92ca66d2e234a6d71651599fa0
Nguyễn Hà Bảo Lâm <blam2010.hn@gmail.com>
2024-03-15 14:34:58 +0700

    Update robots.txt.ts

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,7 +2,7 @@
 
 export const GET: APIRoute = ({ params, request }) => {
   const hostname = new URL(request.url).hostname;
-  if (hostname == "nhblam.space") {
+  if (hostname.includes("nhblam.space")) {
     return new Response(`User-agent: *
 Disallow: /`);
   } else return new Response(``)
