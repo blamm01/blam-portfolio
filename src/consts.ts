f80e2de0acccf61572327856121a14415d30c97e
Nguyễn Hà Bảo Lâm <github@blam.id.vn>
2024-03-15 14:35:30 +0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@ export const Links = {
     discord: 'https://discord.com/users/736636650796351559',
     facebook: 'https://facebook.com/blamm01',
     x: 'https://x.com/blamm01',
-    email: 'mailto:blam@nhblam.space',
+    email: 'mailto:contact@blam.id.vn',
     github: 'https://github.com/blamm01'
 }
