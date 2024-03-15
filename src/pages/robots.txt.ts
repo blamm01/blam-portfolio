aad21ab8b9ec98079e542ae2787ee904d33d33e3
blamm01 <blam2010.hn@gmail.com>
2024-03-15 13:04:52 +0700

    remove microsoft verify file and remove nhblam.space

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,9 @@
+﻿import type { APIRoute } from "astro";
+
+export const GET: APIRoute = ({ params, request }) => {
+  const hostname = new URL(request.url).hostname;
+  if (hostname == "nhblam.space") {
+    return new Response(`User-agent: *
+Disallow: /`);
+  } else return new Response(``)
+};
