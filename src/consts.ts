4862e84559ee4d09d22aa69554fc30c51b603e34
Nguyễn Hà Bảo Lâm <lam@blam.id.vn>
2024-11-04 17:39:15 +0700

    Change Facebook username

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const Links = {
     discord: 'https://discord.com/users/736636650796351559',
-    facebook: 'https://facebook.com/blamm01',
+    facebook: 'https://facebook.com/blamm02',
     x: 'https://x.com/blamm01',
     email: 'mailto:contact@blam.id.vn',
     github: 'https://github.com/blamm01'
