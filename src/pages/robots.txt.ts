093281c0b2fbe928aff8a5ae4e49fee79f674ecc
Nguyễn Hà Bảo Lâm <lam@blam.id.vn>
2024-11-07 23:12:13 +0700

    Delete src/pages/robots.txt.ts

--- src/pages/robots.txt.ts
+++
@@ -1,9 +0,0 @@
-﻿import type { APIRoute } from "astro";
-
-export const GET: APIRoute = ({ params, request }) => {
-  const hostname = new URL(request.url).hostname;
-  if (hostname.includes("nhblam.space")) {
-    return new Response(`User-agent: *
-Disallow: /`);
-  } else return new Response(``)
-};
